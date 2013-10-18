c38cedd3327ec99655f2da9d3a8b16040274df71
Igor Kuznetsov <igkuznetsov@gmail.com>
2013-10-18 18:18:08 +0400

    Now georegions primitives are supported

--- lib/yandex_market_api_client/client.rb
+++ lib/yandex_market_api_client/client.rb
@@ -18,10 +18,20 @@ module YandexMarketApiClient
 
     get :categories, "/v#{config.api_version}/category.#{format}" do |resource|
       resource.optional :page, :count, :sort
+      resource.required :geo_id
     end
 
     get :category, "/v#{config.api_version}/category/{category_id}.#{format}" do |resource|
       resource.optional :page, :count, :sort
+      resource.required :geo_id
+    end
+
+    get :regions, "/v#{config.api_version}/georegion.#{format}" do |resource|
+      resource.optional :page, :count
+    end
+
+    get :region, "/v#{config.api_version}/georegion/{geo_id}.#{format}" do |resource|
+      resource.optional :page, :count
     end
   end
 end
